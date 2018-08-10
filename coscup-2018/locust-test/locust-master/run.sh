f78c4d3076d5a9d1e1430f78458112de8dfa8b6a
fieliapm <fieliapm@gmail.com>
2018-08-11 01:53:09 +0800

    add Docker for Mac support

--- coscup-2018/locust-test/locust-master/run.sh
+++ coscup-2018/locust-test/locust-master/run.sh
@@ -1,3 +1,4 @@
 #!/bin/sh
 
 locust -f dummy.py --master --master-bind-host=`/sbin/ip route|awk '/docker0/ { print $9 }'` --master-bind-port=5557
+#locust -f dummy.py --master --master-bind-host=0.0.0.0 --master-bind-port=5557
